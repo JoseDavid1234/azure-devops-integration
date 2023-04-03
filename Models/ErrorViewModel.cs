cf6f3160e11ced921e8c769ff6b61b9217417044
josdaperales <josdaperales@gmail.com>
2023-04-03 10:56:13 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace azureDevOpsIntegration.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
